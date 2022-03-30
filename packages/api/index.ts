c4376eb9ef3a5cf985e0e91d69b5942f9f32ea49
Alfred Melson <avm@hmtgp.com>
2022-03-31 02:58:54 +0300

    inital api queries, add UUID pkg

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,13 +1,52 @@
-import express, { Response } from 'express'
+import express, { Request, Response } from 'express'
 import * as trpc from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import cors from 'cors'
+import { z } from 'zod'
+import { v4 as uuidv4 } from 'uuid'
+import { IUser } from './model/users'
+import users from './model/users.json'
 
-const appRouter = trpc.router().query('hello', {
-  resolve() {
-    return 'Hello Insly'
+const userDB = {
+  allUsers: users,
+  setAllUsers: function (data: IUser[]) {
+    this.allUsers = data
   }
-})
+}
+
+const appRouter = trpc
+  .router()
+  .query('hello', {
+    resolve() {
+      return 'Hello world III'
+    }
+  })
+  // read all users
+  .query('getUsers', {
+    // initial input value set to 10 by default
+    input: z.number().default(10),
+    resolve({ input }: any) {
+      // slice the last 10 users
+      return userDB.allUsers.slice(-input)
+    }
+  })
+  // create user
+  .mutation('addUser', {
+    input: z.object({
+      firstname: z.string(),
+      lastname: z.string(),
+      email: z.string(),
+      street: z.string(),
+      city: z.string(),
+      country: z.string()
+    }),
+    resolve({ input }: Record<string, any>) {
+      const { firstname, lastname, email, street, city, country } = input
+      const userId: string = uuidv4()
+      users.push({ userId, firstname, lastname, email, street, city, country })
+      return input
+    }
+  })
 
 export type AppRouter = typeof appRouter
 
@@ -23,7 +62,7 @@ app.use(
   })
 )
 
-app.get('/', (res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello from api-server')
 })
 
