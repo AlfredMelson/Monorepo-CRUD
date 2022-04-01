00fce14b5298219af24a63d852dba39d9d1613b6
Alfred Melson <avm@hmtgp.com>
2022-04-01 15:05:07 +0300

    animations and css

--- packages/client/src/style/animations.ts
+++ packages/client/src/style/animations.ts
@@ -10,3 +10,86 @@ export const layoutContainer = {
   },
   exit: { opacity: 0 }
 }
+
+export const usersCard = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.6, -0.05, 0.01, 0.99],
+      delayChildren: 0.4,
+      staggerChildren: 0.2
+    }
+  },
+  exit: { opacity: 0 }
+}
+
+export const usersCardHeading = {
+  initial: { y: 20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99]
+    }
+  }
+}
+
+export const usersCardTagline = {
+  initial: { y: 20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99]
+    }
+  },
+  exit: { y: 20, opacity: 0 }
+}
+
+export const usersCardPanel = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.2
+    }
+  },
+  exit: { opacity: 0 }
+}
+
+export const userListCollocate = {
+  initial: { y: -20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99]
+    }
+  }
+}
+export const userListPaginate = {
+  initial: { y: -20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99]
+    }
+  }
+}
+export const addUserFieldTitle = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.2
+    }
+  },
+  exit: { opacity: 0 }
+}
