76f89f95bf02b6c190b8f59fbd6e3c36c5397e65
Alfred Melson <avm@hmtgp.com>
2022-04-01 15:06:05 +0300

    app layout component

--- packages/client/src/components/layout/AppLayout.tsx
+++ packages/client/src/components/layout/AppLayout.tsx
@@ -1,16 +1,17 @@
 import { motion } from 'framer-motion'
 import { Outlet } from 'react-router-dom'
 import { layoutContainer } from '../../style'
+import DialogContainer from './DialogContainer'
 
 export default function AppLayout() {
   return (
-    <motion.section
-      className='layout-background'
-      variants={layoutContainer}
-      initial='initial'
-      animate='animate'
-      exit='exit'>
-      <Outlet />
-    </motion.section>
+    <div className='bg-[url("/assets/bg-user-pattern.png")] bg-fixed bg-top'>
+      <div className='scrollbar-track-gray-100 scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800 scrollbar scrollbar-thin flex min-h-screen w-screen flex-col'>
+        <motion.section variants={layoutContainer} initial='initial' animate='animate' exit='exit'>
+          <Outlet />
+          <DialogContainer />
+        </motion.section>
+      </div>
+    </div>
   )
 }
