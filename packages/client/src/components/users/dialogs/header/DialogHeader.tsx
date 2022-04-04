4ca20677120fe94bc193c6afa65b7234b684ed5e
Alfred Melson <avm@hmtgp.com>
2022-04-04 09:57:51 +0300

    added dialog header

--- packages/client/src/components/users/dialogs/header/DialogHeader.tsx
+++ packages/client/src/components/users/dialogs/header/DialogHeader.tsx
@@ -1,11 +1,26 @@
+import { motion } from 'framer-motion'
+import CloseIcon from '../../../icons/CloseIcon'
+
 interface IDialogHeader {
   title: string
+  onClick?: any
 }
 
-export default function DialogHeader({ title }: IDialogHeader) {
+export default function DialogHeader({ title, onClick }: IDialogHeader) {
   return (
-    <div className='m-0 p-0'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ delay: 0.3, duration: 0.3 }}
+      className='flex items-start justify-between px-4 pt-5 pb-0'>
       <h6 className='ml-2 text-2xl text-gold-50'>{title}</h6>
-    </div>
+      <button
+        onClick={onClick}
+        className='background-transparent mr-4  text-blue-500 outline-none transition duration-300 ease-in-out hover:text-blue-400  focus:text-blue-400 focus:outline-none active:text-blue-400'
+        type='button'>
+        <CloseIcon />
+      </button>
+    </motion.div>
   )
 }
