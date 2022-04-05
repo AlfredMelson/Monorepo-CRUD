5fada3d4548424494421b43dafad279d66c2e33a
Alfred Melson <avm@hmtgp.com>
2022-04-05 19:38:57 +0300

    optimise server index

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,10 +1,9 @@
-import express, { Request, response, Response } from 'express'
+import express from 'express'
 import * as trpc from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import cors from 'cors'
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid'
-import { IUser } from './model/users'
 import users from './model/users.json'
 import fsPromises from 'fs/promises'
 import path from 'path'
@@ -18,6 +17,7 @@ const userDB = {
 
 const appRouter = trpc
   .router()
+
   // read all users
   .query('getUsers', {
     resolve() {
@@ -36,9 +36,11 @@ const appRouter = trpc
       country: z.string()
     }),
     resolve({ input }: Record<string, any>) {
-      const { firstname, lastname, email, street, city, country } = input
       const userId: string = uuidv4()
-      const newUser = { userId, firstname, lastname, email, street, city, country }
+      const newUser = {
+        userId,
+        ...input
+      }
       userDB.setAllUsers([...userDB.allUsers, newUser])
       // write the new user to the database
       fsPromises.writeFile(
@@ -53,24 +55,35 @@ const appRouter = trpc
 
 export type AppRouter = typeof appRouter
 
-const app = express()
-app.use(cors())
+async function main() {
+  // express implementation
+  const app = express()
+  // cors implementation
+  app.use(cors())
 
-app.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext: () => null
+  app.use((req, _res, next) => {
+    // request logger
+    console.log('⬅️ ', req.method, req.path, req.body ?? req.query)
+
+    next()
   })
-)
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello from Insly api-server')
-})
+  app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext: () => null
+      // createContext
+    })
+  )
+  app.get('/', (_req, res) => res.send('hello'))
 
-// utilise the port aws provides or 8080
-const port = process.env.PORT || 8080
+  // utilise the port aws provides or 8080
+  const port = process.env.PORT || 8080
 
-app.listen(port, () => {
-  console.log(`api-server listening at http://localhost:${port}`)
-})
+  app.listen(port, () => {
+    console.log(`api-server listening at http://localhost:${port}`)
+  })
+}
+
+main()
