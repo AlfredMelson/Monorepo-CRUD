27687d5f315a9b218f52a3b67b0f88f6d294147d
Alfred Melson <avm@hmtgp.com>
2022-04-07 02:43:57 +0300

    reconfig lerna with useWorkspaces

--- packages/client/src/utils/trpc.ts
+++ packages/client/src/utils/trpc.ts
@@ -1,6 +1,6 @@
 // faciliate communication with the api
+import { AppRouter } from '@insly/api'
 import { createReactQueryHooks } from '@trpc/react'
 // import exposed AppRouter from the api (i.e. type definitions)
-import { AppRouter } from 'trpc-api'
 
 export const trpc = createReactQueryHooks<AppRouter>()
