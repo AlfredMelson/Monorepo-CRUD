e1a44c55e052f88ae2ad3452762af11f1758c4ec
Alfred Melson <avm@hmtgp.com>
2022-04-11 14:55:02 +0300

    user count added

--- packages/client/src/components/users/tagline/Tagline.tsx
+++ packages/client/src/components/users/tagline/Tagline.tsx
@@ -1,12 +1,11 @@
 import { motion } from 'framer-motion'
 import { useRecoilValue } from 'recoil'
-import { DeleteUserDialogStateAtom, searchFieldStateAtom } from '../../../recoil-state'
+import { useUser } from '../../../hooks'
+import { DeleteUserDialogStateAtom } from '../../../recoil-state'
 
 export default function Tagline() {
-  const searchFieldState = useRecoilValue(searchFieldStateAtom)
   const deleteUserDialogState = useRecoilValue(DeleteUserDialogStateAtom)
-
-  const emptySearchField: boolean = searchFieldState === ''
+  const { allUsers } = useUser()
 
   return (
     <>
@@ -18,6 +17,18 @@ export default function Tagline() {
           exit={{ opacity: 0, y: 16, transition: { duration: 0.3, ease: [0.6, 0.66, 0.04, 1] } }}
           className='z-0 auto-rows-auto self-center pb-3 text-2xl font-semibold text-white-50'>
           Current User List
+          <motion.h6
+            layout
+            initial={{ opacity: 0, y: 16 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: { delay: 0.3, duration: 0.4, ease: [0.6, 0.66, 0.04, 1] }
+            }}
+            exit={{ opacity: 0, y: 16, transition: { duration: 0.3, ease: [0.6, 0.66, 0.04, 1] } }}
+            className='z-0 auto-rows-auto self-center pt-1 text-sm font-normal text-white-50'>
+            User Count: {allUsers.length}
+          </motion.h6>
         </motion.h6>
       ) : (
         <motion.h6
